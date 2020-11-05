2792ffb3239e3f7be3439d944e13f3fbb306f471
Cray <craymaru@gmail.com>
2020-11-05 19:33:32 +0900

    修正

--- todo_back/app.py
+++ todo_back/app.py
@@ -1,11 +1,21 @@
-from chalice import Chalice
+from chalice import Chalice, NotFoundError
+from chalicelib import database
 
 app = Chalice(app_name='todo_back')
 
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    return database.get_all_todos()
+
+@app.route('/todos/{todo_id}', methods=['GET'])
+def get_todo(todo_id):
+    todo = database.get_todo(todo_id)
+    if todo:
+        return todo
+    else:
+        raise NotFoundError('Todo not found.') #404
+
 
 
 # The view function above will return {"hello": "world"}
--- todo_back/chalicelib/database.py
+++ todo_back/chalicelib/database.py
@@ -1,21 +1,21 @@
-BLUE_ THREE = [
+BLUE_THREE = [
     { 'id': 'L5',
       'title':'夢の舞台へ駆け上がる',
       'memo': 'TONOSAKI',
       'priority': 3,
-      'completed': False,
+      'completed': False
     },
     { 'id': 'L6',
       'title':'今ここで魅せる',
       'memo': 'GENDA',
       'priority': 2,
-      'completed': False,
+      'completed': False
     },
     { 'id': 'L8',
       'title':'その瞬間を掴む',
       'memo': 'KANEKO',
       'priority': 1,
-      'completed': False,
+      'completed': False
     } 
 ]
 
@@ -29,4 +29,4 @@ def get_todo(todo_id):
   for todo in BLUE_THREE:
     if todo['id'] == todo_id:
       return todo
-    return None
+  return None
